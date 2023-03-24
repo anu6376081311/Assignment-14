b232af92105070d22f585fb5266f2cfc03ac2697
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-24 19:55:04 +0530

    Add files via upload

---
+++ A14(3).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,sume=0,sumo=0;;
+    printf("Enter 10 numbers :");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=9;i++)
+    {
+    if(a[i]%2==0)
+    {
+        sume=sume+a[i];
+    }
+    else
+    {
+        sumo=sumo+a[i];
+    }
+    }
+    printf("The sum of even number is %d",sume);
+    printf("\n");
+    printf("the sum off odd number is %d",sumo);
+    return 0;
+}
